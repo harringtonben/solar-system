c94e0ac6b306ebfdc294bc7969405e7ab20c68c6
ben harrington <harringtonben@icloud.com>
2017-09-16 11:12:10 -0500

    finished all styling

--- javascripts/mars.js
+++ javascripts/mars.js
@@ -3,12 +3,15 @@
 const outputMars = () => {
 	let marsData = [
 	{
+	image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Mars_23_aug_2003_hubble.jpg/275px-Mars_23_aug_2003_hubble.jpg",
 	planetname: "Mars",
 	yeardiscovered: "1659",
 	mass: "6.39 × 10^23 kg",
 	size: "2,106 mi",
 	distancetosun: "141.6 million mi",
-	atmosphere: "95% carbon dioxide, 2.7% nitrogen"
+	atmosphere: "95% carbon dioxide, 2.7% nitrogen",
+	satellites: "Too many to list",
+	orbiters: "too many to list"
 	}
 	];
 
--- javascripts/venus.js
+++ javascripts/venus.js
@@ -3,12 +3,15 @@
 const outputVenus = () => {
 	let venusData = [
 	{
+	image: "https://upload.wikimedia.org/wikipedia/commons/8/85/Venus_globe.jpg",
 	planetname: "Venus",
 	yeardiscovered: "1761",
 	mass: "4.867 × 10^24 kg",
 	size: "3,760 mi",
 	distancetosun: "67.24 million mi",
-	atmosphere: "Almost all carbon dioxide"
+	atmosphere: "Almost all carbon dioxide",
+	satellites: "Venus Express",
+	orbiters: "Akatsuki, Mariner 10, Galileo, Hinode"
 	}
 	];
 
